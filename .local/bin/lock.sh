c9ce29490bbffabc1f255acb751e947016394e45
vodchella <twister.kz@gmail.com>
2020-03-29 04:40:13 +0600

    Move skull.png to /usr/share/backgrounds/

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -4,6 +4,8 @@ LAYOUT=$(setxkbmap -query | grep layout | awk '{ print $2 }')
 if [ "$LAYOUT" != "us"  ]; then
     i3-nagbar -t warning -m 'Не стоит блокировать экран при русской раскладке'
 else
+    HEIGHT=$(xrandr | grep \* | awk '{print $1}' | cut -d "x" -f1)
+    CIRCLE_TOP=$((HEIGHT-700))
     scrot /tmp/screen.png
     convert /tmp/screen.png -filter Gaussian -blur 0x8 /tmp/screen.png
     [[ -f $1  ]] && convert /tmp/screen.png -font 'Karumbi' -pointsize 180 -fill white -gravity center -annotate +60+280 "Achtung! Angry Max!" $1 -gravity center -composite -matte /tmp/screen.png
@@ -12,7 +14,7 @@ else
       --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
       --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
       --insidevercolor=fecf4dff --insidewrongcolor=d23c3dff \
-      --ringvercolor=ffffffff --ringwrongcolor=ffffffff --indpos="x+86:y+1003" \
+      --ringvercolor=ffffffff --ringwrongcolor=ffffffff --indpos="x+86:y+${CIRCLE_TOP}" \
       --radius=25 --veriftext="" --wrongtext="" \
       --screen=1 --indicator --clock --verifcolor=9b9b9bff --wrongcolor=9b9b9bff \
       --timepos="x+90:y+920" --timecolor=9b9b9bff --datecolor=9b9b9bff
