a9e8865acb16b7764e4508941200316367488e4e
vodchella <twister.kz@gmail.com>
2020-03-30 21:47:16 +0600

    Remove edit-settings.sh

--- .local/bin/edit-settings.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-SELECTED=$(cat ~/.config/configs.list | sort -ui | rofi -dmenu -theme '~/.local/share/rofi/themes/Arc-Dark.rasi')
-[ ! -z "$SELECTED" ] && tilix -a app-new-window -x vim "${HOME}/${SELECTED}"
