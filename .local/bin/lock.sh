ef7d571c5faaa07d586c7e8e839eea2eff9afa2c
vodchella <twister.kz@gmail.com>
2020-07-31 08:26:15 +0600

    Empty caption in lock screen

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -19,7 +19,7 @@ else
     CIRCLE_TOP=$((HEIGHT/2+PADDING))
     scrot /tmp/screen.png
     convert /tmp/screen.png -filter Gaussian -blur 0x8 /tmp/screen.png
-    [[ -f $1  ]] && convert /tmp/screen.png -font 'Karumbi' -pointsize 180 -fill white -gravity center -annotate +60+280 "Achtung! Angry Max!" $1 -gravity center -composite -matte /tmp/screen.png
+    [[ -f $1  ]] && convert /tmp/screen.png -font 'Karumbi' -pointsize 180 -fill white -gravity center -annotate +60+280 "" $1 -gravity center -composite -matte /tmp/screen.png
     #dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
     i3lock -n -i /tmp/screen.png  \
       --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
