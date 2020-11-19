65cc67ef76d8e1b0f6b67dbcbdfb8476d3558c96
vodchella <twister.kz@gmail.com>
2020-11-19 16:13:14 +0600

    Delay second panel launching and force systray to show within first panel

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -12,6 +12,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+    sleep 1
   done
 else
   polybar -c ~/.config/polybar/config.ini main &
