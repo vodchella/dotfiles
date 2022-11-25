b20d9d1efae0f7c8637f3da869a3232ba2cc4141
vodchella <twister.kz@gmail.com>
2022-11-25 18:06:23 +0600

    Some additional languages to highlight

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -1,14 +1,19 @@
 require'nvim-treesitter.configs'.setup {
     ensure_installed = {
         'bash',
+        'dockerfile',
         'fish',
         'gitignore',
+        'html',
+        'java',
         'json',
         'lua',
         'markdown',
+        'pascal',
         'python',
         'regex',
         'sql',
+        'vim',
         'yaml'
     },
     highlight = {
